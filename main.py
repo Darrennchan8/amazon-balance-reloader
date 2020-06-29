ed6b4681a5fb86a087db54f62d2b8c78e869b506
Darren Chan <darrennchan8@Gmail.com>
2020-06-29 07:33:52 -0400

    Tweak settings for AppEngine deployment.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "Hello World"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
